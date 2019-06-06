95616146f92e93ae5dec3ea54ba8d83aa177f9fc
rise <rise.worlds@outlook.com>
2019-06-06 10:36:39 +0800

    rename

--- android/app/src/main/java/com/rnbip39test/MainApplication.java
+++ android/app/src/main/java/com/rnbip39test/MainApplication.java
@@ -3,7 +3,7 @@ package com.rnbip39test;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.potato.bip39.RNRnBip39Package;
+import com.potato.crypto.PotatoCryptoPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,7 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNRnBip39Package()
+            new PotatoCryptoPackage()
       );
     }
 
